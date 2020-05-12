be586156ae03b0e397119acb837baf775d667155
skjerns <14980558+skjerns@users.noreply.github.com>
2020-05-12 15:53:11 +0200

    update lapse display

--- stimer/stimer.py
+++ stimer/stimer.py
@@ -6,51 +6,57 @@ import os
 
 
 
-def lapse():
+def lapse(prefix=''):
     """
     Will print the time from the previous invocation
     """
     identifier = '%%LAPSE%%'
     if not identifier in starttime:
-        starttime[identifier] = t.time()
+        starttime[identifier] = t.perf_counter()
     else:
-        elapsed = _print_time(t.time() - starttime[identifier])
+        elapsed = _print_time(t.perf_counter() - starttime[identifier])
         caller = getframeinfo(stack()[1][0])
         count = starttime['%%COUND%%']
         line = '{}:{}():{}'.format(os.path.basename(caller.filename), caller.function,  caller.lineno)
         if line in line_cache: 
             line_cache.clear()
-            star='*'
+            line_cache.add(line)
+            star='#'
         else:
             line_cache.add(line)
             star = ''
-        print('[{}] {} - {}\t{}'.format(count, line, elapsed, star))
-        starttime[identifier] = t.time()
+        if prefix != '': prefix = f' {prefix}'
+        print(f'[{count}{prefix}] {line} - {elapsed}\t{star}')
+        starttime[identifier] = t.perf_counter()
     starttime['%%COUND%%']+=1
 
 def start(identifier = ''):
     """
     Starts a timer with the given identifier
     """
-    starttime[identifier] = t.time()
+    starttime[identifier] = t.perf_counter()
    
 
 
-def _print_time(seconds):
-        if seconds > 7200:
-            hours   = seconds//3600
-            minutes = seconds//60 - hours*60
-            return "{:.0f}:{:02.0f} hours".format(hours, minutes)
-        elif seconds > 180:
-            minutes = seconds//60
-            seconds = seconds%60
-            return "{:.0f}:{:02.0f} min".format(minutes , seconds)
-        elif seconds > 1:
-            return "{:02.1f} sec".format(seconds)
-        elif seconds > 0.001:
-            return "{} ms".format(int(seconds*1000))
-        else:
-            return "{} ms".format(seconds*1000)
+def _print_time(seconds):    
+    if seconds > 7200:
+        hours   = seconds//3600
+        minutes = seconds//60 - hours*60
+        return "{:.0f}:{:02.0f} hours".format(hours, minutes)
+    elif seconds > 180:
+        minutes = seconds//60
+        seconds = seconds%60
+        return "{:.0f}:{:02.0f} min".format(minutes , seconds)
+    elif seconds >= 1:
+        return "{:02.1f} sec".format(seconds)
+    elif seconds > 0.01:
+        return "{} ms".format(int(seconds*1000))
+    elif seconds > 0.001:
+        return "{:.1f} ms".format(seconds*1000)
+    elif seconds > 0.0001:
+        return "{:.1f} μs".format(seconds*100000)
+    else:
+        return "{} nanoseconds".format(int(seconds*1e-9))
    
     
 def stop(identifier = ''):
@@ -58,7 +64,7 @@ def stop(identifier = ''):
     Stops a timer with the given identifier and prints the elapsed time
     """
     try:
-        elapsed = t.time()-starttime[identifier]
+        elapsed = t.perf_counter()-starttime[identifier]
         print('Elapsed {}: {}'.format(identifier, _print_time(elapsed)))
         del starttime[identifier]
         return elapsed
