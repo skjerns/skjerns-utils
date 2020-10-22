f0130bf2d3f0790b7e445771e3a7cfe09902cc2b
skjerns <14980558+skjerns@users.noreply.github.com>
2020-10-22 18:00:19 +0200

    added calling of module possible

--- stimer/__init__.py
+++ stimer/__init__.py
@@ -1,2 +1,13 @@
+import sys
+import types
 from .stimer import start, stop, sleep, _dummy_wrapper, lapse
 
+class CallAbleModule(types.ModuleType):
+    def __init__(self):
+        types.ModuleType.__init__(self, __name__)
+        # or super().__init__(__name__) for Python 3
+        self.__dict__.update(sys.modules[__name__].__dict__)
+    def __call__(self):
+        return 42
+
+sys.modules[__name__] = CallAbleModule()
